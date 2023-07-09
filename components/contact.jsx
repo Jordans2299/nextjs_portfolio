3fd18ae604cad18ca1c59ba2e6e828cd8e8154e9
Jordan Stone <jordans2299@gmail.com>
2023-07-09 12:58:00 -0500

    changed color scheme

--- components/contact.jsx
+++ components/contact.jsx
@@ -9,17 +9,17 @@ export default function Contact() {
                     <h1>Contact</h1>
                     <h6>Let's work together</h6>
                 </div>
-                
-                <form method="POST" data-netlify="true" className={styles.contactForm} name="contact">
-                <input type="hidden" name="form-name" value="contact" />
 
-                    <label for="name">Name: </label>
+                <form method="POST" data-netlify="true" className={styles.contactForm} name="contact">
+                    <input type="hidden" name="form-name" value="contact" />
+
+                    <label for="name">Name </label>
                     <input type="text" id="name" name="name" placeholder="Enter your name..." required />
-                    <label for="email">Email: </label>
+                    <label for="email">Email </label>
                     <input type="email" id="email" name="email" placeholder="Enter your email..." required />
-                    <label for="subject">Subject: </label>
+                    <label for="subject">Subject </label>
                     <textarea name="subject" id="subject" cols="10" rows="10"></textarea>
-                    <input type="submit" value="Submit" name="submit" />
+                    <input className={styles.submitBtn} type="submit" value="Submit" name="submit" />
                 </form>
             </div>
         </div>
